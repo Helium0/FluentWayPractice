e939e092cd375ec1fc40274e74e4d7180c953aea
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-19 23:35:24 +0200

    Added more tests in LoginTests class

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -14,13 +14,15 @@ public class LoginTests extends BasePage {
 
     private HomePage homePage;
     private LoginPage loginPage;
+    private WebDriver driver;
     private final By USER_NAME_ERROR = By.xpath("//span[@for='UserName']");
     private final By USER_PASSWORD_ERROR = By.xpath("//span[@for='Password']");
+    private final By INVALID_LOGIN_ERROR = By.xpath("//div[@class='validation-summary-errors text-danger']");
 
 
     @Test
     public void userCantLoginWithoutCredentials() {
-        WebDriver driver = DriverManagement.getDriver();
+        driver = DriverManagement.getDriver();
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
         homePage.clickLogin();
@@ -28,7 +30,50 @@ public class LoginTests extends BasePage {
 
         Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
         Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
+    }
 
+    @Test
+    public void userCantLoginWithIncorrectCredentials() {
+        driver = DriverManagement.getDriver();
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+        homePage.clickLogin();
+        loginPage.loginUserToAccount("Patryk","Tester666");
+
+        Assert.assertEquals(driver.findElement(INVALID_LOGIN_ERROR).getText(), "Invalid login attempt.");
+    }
+
+    @Test
+    public void userCantLoginWithoutPassword() {
+        driver = DriverManagement.getDriver();
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+        homePage.clickLogin();
+        loginPage.loginUserToAccount("Patryk","");
+
+        Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
+    }
+
+    @Test
+    public void userCantLoginWithoutUserName() {
+        driver = DriverManagement.getDriver();
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+        homePage.clickLogin();
+        loginPage.loginUserToAccount("","Tester666");
+
+        Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
+    }
+
+    @Test
+    public void userCanClickOnRememberCheckbox() {
+        driver = DriverManagement.getDriver();
+        homePage = new HomePage(driver);
+        loginPage = new LoginPage(driver);
+        homePage.clickLogin();
+        loginPage.clickOnRememberMeCheckbox();
+
+        Assert.assertTrue(loginPage.getRememberMeCheckbox().isSelected(), "Checkbox is not selected");
     }
 
 }

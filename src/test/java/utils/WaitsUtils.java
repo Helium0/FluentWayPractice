395945820c86114381e80eda2e1d36b817e73391
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-20 19:57:19 +0200

    Created more wait methods

--- src/test/java/utils/WaitsUtils.java
+++ src/test/java/utils/WaitsUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,10 +12,18 @@ public class WaitsUtils {
 
 
 
+    private static WebDriverWait webDriverWait(WebDriver driver) {
+        return new WebDriverWait(driver,Duration.ofSeconds(10));
+    }
+
 
     public static void waitForByElement(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        webDriverWait(driver).until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+
+    public static void waitForWebelement(WebDriver driver, WebElement element) {
+        webDriverWait(driver).until(ExpectedConditions.visibilityOf(element));
     }
 
 

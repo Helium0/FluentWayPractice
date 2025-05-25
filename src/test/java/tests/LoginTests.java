37fe27fa44d8197677e29765851f619aa3beec61
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-25 23:47:50 +0200

    Implemented logger for each test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import factory.DriverManagement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -15,6 +17,7 @@ public class LoginTests extends BasePage {
     private NavigationBarPage navigationBarPage;
     private LoginPage loginPage;
     private WebDriver driver;
+    private static final Logger logger = LogManager.getLogger(LoginTests.class);
     private final By USER_NAME_ERROR = By.xpath("//span[@for='UserName']");
     private final By USER_PASSWORD_ERROR = By.xpath("//span[@for='Password']");
     private final By INVALID_LOGIN_ERROR = By.xpath("//div[@class='validation-summary-errors text-danger']");
@@ -22,58 +25,94 @@ public class LoginTests extends BasePage {
 
     @Test
     public void userCantLoginWithoutCredentials() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage = new LoginPage(driver);
         navigationBarPage.clickLogin();
         loginPage.loginUserToAccount("","");
+        try {
+            Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
+            Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
+            logger.info("*** TEST name: userCantLoginWithoutCredentials PASSED ***");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: userCantLoginWithoutCredentials FAILED", e);
+            throw e;
+        }
 
-        Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
-        Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
     }
 
     @Test
     public void userCantLoginWithIncorrectCredentials() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage = new LoginPage(driver);
         navigationBarPage.clickLogin();
         loginPage.loginUserToAccount("Patryk","Tester666");
 
-        Assert.assertEquals(driver.findElement(INVALID_LOGIN_ERROR).getText(), "Invalid login attempt.");
+        try {
+            Assert.assertEquals(driver.findElement(INVALID_LOGIN_ERROR).getText(), "Invalid login attempt.");
+            logger.info("*** TEST name: userCantLoginWithIncorrectCredentials PASSED ***");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: userCantLoginWithIncorrectCredentials FAILED", e);
+            throw e;
+        }
+
     }
 
     @Test
     public void userCantLoginWithoutPassword() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage = new LoginPage(driver);
         navigationBarPage.clickLogin();
         loginPage.loginUserToAccount("Patryk","");
 
-        Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
+        try {
+            Assert.assertEquals(driver.findElement(USER_PASSWORD_ERROR).getText(), "The Password field is required.");
+            logger.info("*** TEST name: userCantLoginWithoutPassword PASSED ***");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: userCantLoginWithoutPassword FAILED", e);
+            throw e;
+        }
     }
 
     @Test
     public void userCantLoginWithoutUserName() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage = new LoginPage(driver);
         navigationBarPage.clickLogin();
         loginPage.loginUserToAccount("","Tester666");
 
-        Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
+        try {
+            Assert.assertEquals(driver.findElement(USER_NAME_ERROR).getText(), "The UserName field is required.");
+            logger.info("*** TEST name: userCantLoginWithoutUserName PASSED ***");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: userCantLoginWithoutUserName FAILED", e);
+            throw e;
+        }
     }
 
     @Test
     public void userCanClickOnRememberCheckbox() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         navigationBarPage = new NavigationBarPage(driver);
         loginPage = new LoginPage(driver);
         navigationBarPage.clickLogin();
         loginPage.clickOnRememberMeCheckbox();
 
-        Assert.assertTrue(loginPage.getRememberMeCheckbox().isSelected(), "Checkbox is not selected");
+        try {
+            Assert.assertTrue(loginPage.getRememberMeCheckbox().isSelected(), "Checkbox is not selected");
+            logger.info("*** TEST name: userCanClickOnRememberCheckbox PASSED ***");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: userCanClickOnRememberCheckbox FAILED", e);
+            throw e;
+        }
     }
 
 }

9248b04b3fcb346c7a90c2dd5d3e3943293df756
Patryk Prentki <patryk.prentki90@gmail.com>
2025-05-25 23:59:55 +0200

    Implemented logger in HomePageTests for each test

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -2,6 +2,8 @@ package tests;
 
 import factory.BasePage;
 import factory.DriverManagement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -13,51 +15,80 @@ public class HomePageTests extends BasePage {
 
     private WebDriver driver;
     private HomePage homePage;
+    private Logger logger = LogManager.getLogger(HomePageTests.class);
     private final By NEXT_PAGE_INFORMARION = By.xpath("//div[@id='main-message']//h1");
     private final By ACCEPT_YOUTUBE_COOKIES = By.xpath("//span[contains(text(),'Accept all')]");
     private final By YOUTUBE_LOGO = By.xpath("//yt-icon[@id='logo-icon']");
 
     @Test
     public void checkVisitNowFunctionality() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         homePage = new HomePage(driver);
         homePage.clickOnVisitNowButton();
 
-        Assert.assertEquals(driver.findElement(NEXT_PAGE_INFORMARION).getText(), "This site can’t be reached");
+        try {
+            Assert.assertEquals(driver.findElement(NEXT_PAGE_INFORMARION).getText(), "This site can’t be reached");
+            logger.info("*** TEST name: checkVisitNowFunctionality PASSED");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: checkVisitNowFunctionality FAILED", e);
+        }
     }
 
     @Test
     public void checkLearnMoreButtonFunctionality() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         homePage = new HomePage(driver);
         homePage.clickOnLearnMoreButton();
 
-        Assert.assertEquals(driver.findElement(NEXT_PAGE_INFORMARION).getText(), "This site can’t be reached");
+        try {
+            Assert.assertEquals(driver.findElement(NEXT_PAGE_INFORMARION).getText(), "This site can’t be reached");
+            logger.info("*** TEST name: checkLearnMoreButtonFunctionality PASSED");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: checkLearnMoreButtonFunctionality FAILED", e);
+        }
     }
 
     @Test
     public void checkCoursesButtonsFunctionality() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         homePage = new HomePage(driver);
         homePage.clickOnUdemyButton();
 
-        Assert.assertEquals(driver.getTitle(),"Karthik KK | Software Architect, Consultant,Youtuber and Best Seller" +
-                " | Udemy");
+        try {
+            Assert.assertEquals(driver.getTitle(),"Karthik KK | Software Architect, Consultant,Youtuber and Best Seller" +
+                    " | Udemy");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: checkCoursesButtonsFunctionality FAILED", e);
+        }
         driver.navigate().back();
         homePage.clickOnYoutubeButton();
         driver.findElement(ACCEPT_YOUTUBE_COOKIES).click();
         WaitsUtils.waitForWebelement(driver, driver.findElement(YOUTUBE_LOGO));
 
-        Assert.assertEquals(driver.getTitle(),"Execute Automation - YouTube");
+        try {
+            Assert.assertEquals(driver.getTitle(),"Execute Automation - YouTube");
+            logger.info("*** TEST name: checkCoursesButtonsFunctionality PASSED");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: checkCoursesButtonsFunctionality FAILED", e);
+        }
     }
 
     @Test
     public void checkSourceCodeButton() {
+        logger.info("*** START ***");
         driver = DriverManagement.getDriver();
         homePage = new HomePage(driver);
         homePage.clickOnGetSourceButton();
 
-        Assert.assertEquals(driver.getTitle(), "executeautomation (ExecuteAutomation) · GitHub");
+        try {
+            Assert.assertEquals(driver.getTitle(), "executeautomation (ExecuteAutomation) · GitHub");
+            logger.info("*** TEST name: checkSourceCodeButton PASSED");
+        } catch (AssertionError e) {
+            logger.error("*** TEST name: checkSourceCodeButton FAILED", e);
+        }
     }
 
 }
